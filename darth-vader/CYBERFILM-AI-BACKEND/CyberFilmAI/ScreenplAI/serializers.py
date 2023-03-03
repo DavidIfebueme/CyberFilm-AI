82c8ee33f84f6af53748bebf4891b2d400c84151
DavidIfebueme <davidifebueme@gmail.com>
2023-03-03 12:58:55 +0100

    More updates

--- darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/serializers.py
+++ darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/serializers.py
@@ -4,4 +4,11 @@ from .models import PlotInputFields
 class PlotInputFieldsSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlotInputFields
-        fields = ('id', 'title', 'logline', 'Themes', 'StoryTypes', 'Genres', 'Tone', 'Audience', 'B_Story', 'Setting')
+        fields = ('id', 'title', 'logline', 'Themes', 'StoryTypes',
+         'Genres', 'Tone', 'Audience', 'B_Story', 'Setting')
+
+class CharacterInputFieldsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CharacterInputFields
+        fields = ('name', 'character_arc', 'description', 'personality',
+         'archetypes', 'want', 'need', 'flaw','strength', 'skills' )
--- darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/views.py
+++ darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .serializers import PlotInputFieldsSerializer
+from rest_framework import generics, status
+from .serializers import PlotInputFieldsSerializer, CharacterInputFieldsSerializer
 from .models import *  
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
 class IndexView(generics.ListAPIView):
     queryset = PlotInputFields.objects.all() 
     serializer_class = PlotInputFieldsSerializer
+'''
+class CreateGhostWriterView(APIView):
+
+    serializer_class = CharacterInputFieldsSerializer, PlotInputFieldsSerializer
+    def post(self, request, format=None):
+        if not self.request.session.exists(self.request.session.session_key):
+            self.request.session.create()
+
+        serializer = self.serializer_class(data=request.data)
+        #if serializer.is_valid():
+
+'''
+
+
