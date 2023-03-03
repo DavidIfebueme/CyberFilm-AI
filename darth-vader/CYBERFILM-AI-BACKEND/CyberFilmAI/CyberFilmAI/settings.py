35e96ca113a323803461c1d1c35790120adcbbc4
DavidIfebueme <davidifebueme@gmail.com>
2023-03-03 03:04:05 +0100

    First commit for django app frontend with react

--- darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/CyberFilmAI/settings.py
+++ darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/CyberFilmAI/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'ScreenplAI.apps.ScreenplaiConfig',
+    #'rest_framwework',
+    'frontend.apps.FrontendConfig'
 ]
 
 MIDDLEWARE = [
--- darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/urls.py
+++ darth-vader/CYBERFILM-AI-BACKEND/CyberFilmAI/ScreenplAI/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib import admin
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('ScreenplAI', include('ScreenplAI.urls')),
+    path('', include('frontend.urls'))
+
 ] 
